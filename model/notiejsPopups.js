f58518884338d1c55cfb0dea970b2b43f1e9b381
Gerard <gerardortizgracia@gmail.com>
2024-02-15 20:27:28 +0100

    correcto funcionamiento de notejs

--- model/notiejsPopups.js
+++ model/notiejsPopups.js
@@ -1,7 +1,5 @@
-import notie from 'notie';
-
 notie.alert({
-    type: 'info', // optional, default = 4, enum: [1, 2, 3, 4, 5, 'success', 'warning', 'error', 'info', 'neutral']
-    text: 'TESTING',
-    stay: true, // optional, default = false
-  })
+  type: 1, // optional, default = 4, enum: [1, 2, 3, 4, 5, 'success', 'warning', 'error', 'info', 'neutral']
+  text: '¡Pedido realizado correctamente!',
+  time: 10, // optional, default = 3, minimum = 1,
+})
